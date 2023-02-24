b4c4874ba67ddc36fa8af70a5dd660590c55aa07
HenryLima07 <00033721@uca.edu.sv>
2023-02-24 16:02:19 -0600

    :lipstick: add Container structure

---
+++ src/componentes/Container.component.tsx
@@ -0,0 +1,9 @@
+import classes from "./Container.module.scss";
+
+const Container = ({children, className, ...rest})=>{
+    return(
+        <div className={`${classes.Container} ${className}`}>
+            {children}
+        </div>  
+    );
+}
