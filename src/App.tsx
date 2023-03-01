725ca441f45ce677da660a590027e85fffd6480e
HenryLima07 <00033721@uca.edu.sv>
2023-02-28 18:22:51 -0600

    update project's elements

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import reactLogo from './assets/react.svg'
 import classes from "./app.module.scss";
 import "./index.css";
 
+import Header from './componentes/Header/Header.component';
+
 function App() {
 
   return (
     <div className={classes["app"]}>
+      <Header />
 
     </div>
   )
