d7d7db283945bc5fea5a6d30a2ff48f707c5bc4b
HenryLima07 <00033721@uca.edu.sv>
2023-02-28 18:49:07 -0600

    add routing views

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Route, Routes} from "react-router-dom";
+
 import classes from "./app.module.scss";
 import "./index.css";
 
 import Header from './componentes/Header/Header.component';
+
+
+//importing views
+import LandingView from './views/Landing-view/Landing.view';
 
 function App() {
 
@@ -11,8 +15,11 @@ function App() {
     <div className={classes["app"]}>
       <Header />
 
+      <Routes>
+        <Route path="/" element={<LandingView />} />
+      </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
