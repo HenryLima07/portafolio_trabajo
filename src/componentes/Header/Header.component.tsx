70aa3ba934532f1d01f728be5ef42edf7b9c865d
HenryLima07 <00033721@uca.edu.sv>
2023-02-28 18:21:45 -0600

    :lipstick: add Header element

--- src/componentes/Header/Header.component.tsx
+++ src/componentes/Header/Header.component.tsx
@@ -1,8 +1,20 @@
 import classes from "./Header.module.scss";
+import {Link} from "react-router-dom";
 
+//elements
+import logo from "../../assets/HL_log.png";
+import Container from "../Container.component";
+
+//<Link to={"./some.html"}>link to another</Link>
 const Header = ()=>{
     return(
-        <div></div>
+        <header>
+            <Container>
+                <Link to={"/"}>
+                    <img src={logo} alt="Henry Lima's logo" />
+                </Link>
+            </Container>
+        </header>
     );
 }
 
